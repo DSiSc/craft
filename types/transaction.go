27803b4aa2348c83d74052eec1bbd668b0c2e6e6
walterBishop <walterkangluo@gmail.com>
2018-08-23 23:29:46 +0800

    feature# Add NewTransaction() to create a transaction

--- types/transaction.go
+++ types/transaction.go
@@ -58,3 +58,43 @@ func (tx *Transaction) SerializeUnsigned(w io.Writer) error {
 
 	return nil
 }
+
+func CopyBytes(b []byte) (copiedBytes []byte) {
+	if b == nil {
+		return nil
+	}
+	copiedBytes = make([]byte, len(b))
+	copy(copiedBytes, b)
+
+	return
+}
+
+// New a transaction
+func newTransaction(nonce uint64, to *Address, amount *big.Int, gasLimit uint64, gasPrice *big.Int, data []byte) *Transaction {
+	if len(data) > 0 {
+		data = CopyBytes(data)
+	}
+	d := txdata{
+		AccountNonce: nonce,
+		Recipient:    to,
+		Payload:      data,
+		Amount:       new(big.Int),
+		GasLimit:     gasLimit,
+		Price:        new(big.Int),
+		V:            new(big.Int),
+		R:            new(big.Int),
+		S:            new(big.Int),
+	}
+	if amount != nil {
+		d.Amount.Set(amount)
+	}
+	if gasPrice != nil {
+		d.Price.Set(gasPrice)
+	}
+
+	return &Transaction{data: d}
+}
+
+func NewTransaction(nonce uint64, to Address, amount *big.Int, gasLimit uint64, gasPrice *big.Int, data []byte) *Transaction {
+	return newTransaction(nonce, &to, amount, gasLimit, gasPrice, data)
+}
--- types/transaction_test.go
+++ types/transaction_test.go
@@ -3,6 +3,7 @@ package types
 import (
 	"bytes"
 	"github.com/stretchr/testify/assert"
+	"math/big"
 	"testing"
 )
 
@@ -16,4 +17,19 @@ func Test_Hash(t *testing.T) {
 	buf := bytes.Buffer{}
 	err := tx.SerializeUnsigned(&buf)
 	assert.Nil(err)
+}
+
+func Test_NewTransaction(t *testing.T) {
+	assert := assert.New(t)
+	b := Address{
+		0xb2, 0x6f, 0x2b, 0x34, 0x2a, 0xab, 0x24, 0xbc, 0xf6, 0x3e,
+		0xa2, 0x18, 0xc6, 0xa9, 0x27, 0x4d, 0x30, 0xab, 0x9a, 0x15,
+	}
+	emptyTx := NewTransaction(
+		0,
+		b,
+		big.NewInt(0), 0, big.NewInt(0),
+		nil,
+	)
+	assert.NotNil(emptyTx)
 }
