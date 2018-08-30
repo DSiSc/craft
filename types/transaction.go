1ca1f59b3ee03e9b5faec7cee25dbdcbd1e5a989
bchain618 <bchain618@gmail.com>
2018-08-30 21:04:12 +0800

    feature#add Message type

--- types/transaction.go
+++ types/transaction.go
@@ -105,3 +105,37 @@ func newTransaction(nonce uint64, to *Address, amount *big.Int, gasLimit uint64,
 func NewTransaction(nonce uint64, to Address, amount *big.Int, gasLimit uint64, gasPrice *big.Int, data []byte) *Transaction {
 	return newTransaction(nonce, &to, amount, gasLimit, gasPrice, data)
 }
+
+// Message evm context message
+type Message struct {
+	to         *Address
+	from       Address
+	nonce      uint64
+	amount     *big.Int
+	gasLimit   uint64
+	gasPrice   *big.Int
+	data       []byte
+	checkNonce bool
+}
+
+func NewMessage(from Address, to *Address, nonce uint64, amount *big.Int, gasLimit uint64, gasPrice *big.Int, data []byte, checkNonce bool) Message {
+	return Message{
+		from:       from,
+		to:         to,
+		nonce:      nonce,
+		amount:     amount,
+		gasLimit:   gasLimit,
+		gasPrice:   gasPrice,
+		data:       data,
+		checkNonce: checkNonce,
+	}
+}
+
+func (m Message) From() Address { return m.from }
+func (m Message) To() *Address  { return m.to }
+func (m Message) GasPrice() *big.Int  { return m.gasPrice }
+func (m Message) Value() *big.Int     { return m.amount }
+func (m Message) Gas() uint64         { return m.gasLimit }
+func (m Message) Nonce() uint64       { return m.nonce }
+func (m Message) Data() []byte        { return m.data }
+func (m Message) CheckNonce() bool    { return m.checkNonce }
