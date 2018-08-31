d9e0fa5aa2448f2be5c3dcd2a3475c3adb1127e9
walterBishop <walterkangluo@gmail.com>
2018-08-31 11:09:28 +0800

    feature# Add methods of account to implement Sign.

--- types/account.go
+++ types/account.go
@@ -10,3 +10,19 @@ type Account struct {
 	Address    Address
 	SigScheme  keypair.SignatureScheme
 }
+
+func (*Account) PrivKey() keypair.PrivateKey {
+	return nil
+}
+
+//get signer's public key
+
+func (*Account) PubKey() keypair.PublicKey {
+	return nil
+}
+
+func (*Account) Scheme() keypair.SignatureScheme {
+	//var temp keypair.SignatureScheme
+	var byt byte = 'a'
+	return keypair.SignatureScheme(byt)
+}
