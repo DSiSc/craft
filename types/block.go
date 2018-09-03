6227a839b2203715355b060446477969c6cf7016
walterBishop <walterkangluo@gmail.com>
2018-09-03 22:36:46 +0800

    feature# Adjust Block sturct.
    1. Delete BlockHash from Header struct.
    2. Add HeadHash for Block struct.
    3. Move SigData from Header to Block, and change it with
    map[Address][]byte.

--- types/block.go
+++ types/block.go
@@ -6,4 +6,6 @@ package types
 type Block struct {
 	Header       *Header
 	Transactions []*Transaction
+	HeaderHash   Hash     `json:"headerHash"    gencodec:"required"`
+	SigData      [][]byte `json:"signData"    gencodec:"required"`
 }
