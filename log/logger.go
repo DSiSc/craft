ea49cc20fc743ff85c474e876a75585174bf8098
hoticode <haotian.coder@gmail.com>
2018-09-03 16:46:24 +0800

    feature# add logging package
    - use adapter so that backend logger can be changeable, use zerolog for now
    - use global variables to make usage and config easy

---
+++ log/logger.go
@@ -0,0 +1,21 @@
+package log
+
+// Logger interface defines all behaviors of a backendLogger.
+type Logger interface {
+	Debug(msg string)
+	Info(msg string)
+	Warn(msg string)
+	Error(msg string)
+	Fatal(msg string)
+	Panic(msg string)
+
+	DebugKV(msg string, keyvals map[string]interface{})
+	InfoKV(msg string, keyvals map[string]interface{})
+	WarnKV(msg string, keyvals map[string]interface{})
+	ErrorKV(msg string, keyvals map[string]interface{})
+	FatalKV(msg string, keyvals map[string]interface{})
+	PanicKV(msg string, keyvals map[string]interface{})
+
+	SetGlobalLogLevel(level Level)
+	SetOutputFlags(flags *OutputFlags)
+}
