bd272ec1f8a0cae184b2dbaa461bcd381d99ebb9
walterBishop <walterkangluo@gmail.com>
2018-09-03 20:37:38 +0800

    feature# Add event type and structure.
    1. Event.go define a event center.

---
+++ types/event.go
@@ -0,0 +1,29 @@
+package types
+
+type EventType uint8
+
+const (
+	EventBlockCommitted EventType = 0
+)
+
+type EventFunc func(v interface{})
+
+type Subscriber chan interface{}
+
+type EventCenter interface {
+
+	// subscriber subscribe specified eventType with eventFunc
+	Subscribe(eventType EventType, eventFunc EventFunc) Subscriber
+
+	// subscriber unsubscribe specified eventType
+	UnSubscribe(eventType EventType, subscriber Subscriber) (err error)
+
+	// notify subscriber of eventType
+	Notify(eventType EventType, value interface{}) (err error)
+
+	// notify specified eventFunc
+	NotifySubscriber(eventFunc EventFunc, value interface{})
+
+	// notify subscriber traversing all events
+	NotifyAll() (errs []error)
+}
