caf4f87d655ff3d8759c1f4a4f303671e0eaf811
walterBishop <walterkangluo@gmail.com>
2018-09-21 06:22:36 -0700

    feature# Add ReceiptHash to Header struct.
    1. ReceiptHash record results of txs excuted by EVM.

--- types/header.go
+++ types/header.go
@@ -5,13 +5,13 @@ package types
 
 // Header represents a block header in the Ethereum blockchain.
 type Header struct {
-	ChainID       uint64  `json:"chainId"    gencodec:"required"`     // chainid
-	PrevBlockHash Hash    `json:"prevHash"    gencodec:"required"`    // preblock hash
-	StateRoot     Hash    `json:"stateRoot"    gencodec:"required"`   // statedb root
-	TxRoot        Hash    `json:"txRoot"    gencodec:"required"`      // transactions root
-	ReceiptsRoot  Hash    `json:"receipsRoot"    gencodec:"required"` // receipt root
-	Height        uint64  `json:"height"    gencodec:"required"`      // block height
-	Timestamp     uint64  `json:"timestamp"    gencodec:"required"`   // timestamp
-	MixDigest     Hash    `json:"mixDigest"    gencodec:"required"`   // digest
-	Coinbase      Address `json:"coinbase"    gencodec:"required"`    // coin base
+	ChainID       uint64  `json:"chainId"    gencodec:"required"`       // chainid
+	PrevBlockHash Hash    `json:"prevHash"    gencodec:"required"`      // preblock hash
+	StateRoot     Hash    `json:"stateRoot"    gencodec:"required"`     // statedb root
+	TxRoot        Hash    `json:"txRoot"    gencodec:"required"`        // transactions root
+	ReceiptsRoot  Hash    `json:"receipsRoot"    gencodec:"required"`   // receipt root
+	Height        uint64  `json:"height"    gencodec:"required"`        // block height
+	Timestamp     uint64  `json:"timestamp"    gencodec:"required"`     // timestamp
+	MixDigest     Hash    `json:"mixDigest"    gencodec:"required"`     // digest
+	Coinbase      Address `json:"coinbase"    gencodec:"required"`      // coin base
 }
