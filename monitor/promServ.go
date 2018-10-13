aa5d075bdbb465cc80b254cb8495d23952819b33
hoticode <haotian.coder@gmail.com>
2018-10-13 20:20:35 +0800

    optimize# Add configuration structure for prometheus.

--- monitor/promServ.go
+++ monitor/promServ.go
@@ -8,23 +8,32 @@ import (
 	"net/http"
 )
 
-var promServ *http.Server
+var prometheusServ *http.Server
+
+type PrometheusConfig struct {
+	PrometheusEnabled bool
+	PrometheusPort    string
+	PrometheusMaxConn int
+}
 
 // startPrometheusServer starts a Prometheus HTTP server, listening for metrics
 // collectors on addr.
-func StartPrometheusServer(port string, maxConn int) {
+func StartPrometheusServer(config PrometheusConfig) {
+
+	if !config.PrometheusEnabled {
+		return
+	}
 
 	createMetrics()
 
 	// create prometheus server
-	if promServ == nil {
-		promServ = &http.Server{
-			//Addr: ":" + node.config.PrometheusPort,
-			Addr: ":" + port,
+	if prometheusServ == nil {
+		prometheusServ = &http.Server{
+			Addr: ":" + config.PrometheusPort,
 			Handler: promhttp.InstrumentMetricHandler(
 				prometheus.DefaultRegisterer, promhttp.HandlerFor(
 					prometheus.DefaultGatherer,
-					promhttp.HandlerOpts{MaxRequestsInFlight: maxConn},
+					promhttp.HandlerOpts{MaxRequestsInFlight: config.PrometheusMaxConn},
 				),
 			),
 		}
@@ -32,7 +41,7 @@ func StartPrometheusServer(port string, maxConn int) {
 
 	// start prometheus server
 	go func() {
-		if err := promServ.ListenAndServe(); err != http.ErrServerClosed {
+		if err := prometheusServ.ListenAndServe(); err != http.ErrServerClosed {
 			// Error starting or closing listener:
 			log.Error("Prometheus HTTP server ListenAndServe", "err", err)
 		}
@@ -41,8 +50,8 @@ func StartPrometheusServer(port string, maxConn int) {
 
 // stopPrometheusServer stops a Prometheus HTTP server
 func StopPrometheusServer() {
-	if promServ != nil {
-		if err := promServ.Shutdown(context.Background()); err != nil {
+	if prometheusServ != nil {
+		if err := prometheusServ.Shutdown(context.Background()); err != nil {
 			// Error from closing listeners, or context timeout:
 			log.ErrorKV("Prometheus HTTP server Shutdown", map[string]interface{}{"err": err})
 		}
