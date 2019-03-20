fe0a27afba8f22a8183aeb3b21ed68eae17e9e5e
walterkangluo <walterkangluo@gmail.com>
2019-03-20 11:58:22 +0800

    feature# Add contract type and netadata contract address.

--- types/contract.go
+++ types/contract.go
@@ -5,4 +5,19 @@ const (
 	Null               = "Null"
 	JustitiaRightToken = "JustitiaRight"
 	JustitiaVoting     = "Voting"
+	JustitiaWhiteList  = "WhiteList"
+	JustitiaMetaData   = "MetaData"
 )
+
+type ContractType uint8
+
+const (
+	ContractInitialType       ContractType = iota
+	ContractJustitiaRight
+	ContractVote
+	ContractWhiteList
+	ContractMetaData
+	ConTractMaximumType
+)
+
+const MetaDataContractAddress = "0x8be503bcded90ed42eff31f56199399b2b0154ca"
