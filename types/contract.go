8804f5e41600127a2b2b34fd52bc925597fce503
StupidTAO <2310852380@qq.com>
2019-08-01 10:11:31 +0800

    future# add crossTransaction const

--- types/contract.go
+++ types/contract.go
@@ -7,6 +7,7 @@ const (
 	JustitiaVoting     = "Voting"
 	JustitiaWhiteList  = "WhiteList"
 	JustitiaMetaData   = "MetaData"
+	JustitiaCrossFundsPool = "CrossFundsPool"
 )
 
 type ContractType int
@@ -17,6 +18,7 @@ const (
 	VoteContractType
 	WhiteListContractType
 	MetaDataContractType
+	CrossFundsPoolContractType
 	MaximumContractType
 )
 
@@ -26,4 +28,5 @@ const (
 	JustiitaContractDefaultAddress      = "bd770416a3345f91e4b34576cb804a576fa48eb1"
 	VotingContractDefaultAddress        = "5a443704dd4b594b382c22a083e2bd3090a6fef3"
 	WhiteListContractTypeDefaultAddress = "47e9fbef8c83a1714f1951f142132e6e90f5fa5d"
+	CrossFundsPoolDefaultAddress        = "47c5e40890bce4a473a49d7501808b9633f29782"
 )
